b2bb3674ce02d4051f0dc3442f37467bd6604b50
JaponBaligi <f.123lee123@gmail.com>
2026-01-24 20:59:31 +0300

    fixed build loop

--- src/class_detector.gd
+++ src/class_detector.gd
@@ -1,4 +1,4 @@
-extends Reference
+extends RefCounted
 class_name ClassDetector
 
 # class definition detector
--- src/confidence_calculator.gd
+++ src/confidence_calculator.gd
@@ -1,4 +1,4 @@
-extends Reference
+extends RefCounted
 class_name ConfidenceCalculator
 
 # confidence score calculator
